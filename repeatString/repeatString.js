bd60a13d9d59f54c887f45b3fbb080ca78aa7a2d
Rebecca Hawkenson <rehawkenson@gmail.com>
2020-07-01 15:16:49 -0500

    reset activity to origin to deal with Jasmine-related terminal errors

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,9 +1,5 @@
-let string;
+const repeatString = function() {
 
-const repeatString = function(str, repeat) {
-    for (let i = 0; 0 < 4; i++){
-        string += 'hey';
-    }
 }
 
 module.exports = repeatString
