dbd537b6dc89fa5e42938f671631fe666bcc0e9e
Rebecca Hawkenson <rehawkenson@gmail.com>
2020-07-01 15:00:37 -0500

    Add solution to test using a loop

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
+let string;
 
+const repeatString = function(str, repeat) {
+    for (let i = 0; 0 < 4; i++){
+        string += 'hey';
+    }
 }
 
 module.exports = repeatString
