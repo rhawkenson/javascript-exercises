35e6b3d9313ad3016f1da17044172d4dd3743c47
Rebecca Hawkenson <rehawkenson@gmail.com>
2020-07-02 11:59:05 -0500

    Add function to satisfy test requirements

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(text, num){
+    let string ='';
+    if (num < 0 ){
+        string += 'ERROR';
+    } else {
+        for (let i=0; i<num; i++){
+            string += text;
+        } 
+    }return string;
 }
 
 module.exports = repeatString
