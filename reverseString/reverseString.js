5e388e8fcfcc6579d8dfc29cd140ee5202b6c26a
Rebecca Hawkenson <rehawkenson@gmail.com>
2020-07-03 09:20:40 -0500

    Initial Commit

--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(text) {
+    let string ='';
+    for (let i=text.length-1; i>0; i--){
+        string += text.splice([i]);
+    } return string;
 }
 
 module.exports = reverseString
