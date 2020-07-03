ff841645ebcbae8d271f475f98e23b5391a2cbd9
Rebecca Hawkenson <rehawkenson@gmail.com>
2020-07-03 13:51:15 -0500

    Enter function to pass all tests

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,6 +1,15 @@
-const removeFromArray = function() {
-
-}
+const removeFromArray = function (...args) {
+    const newArray = args[0];
+    const finalArray = [];
+    newArray.forEach((item) => {
+        if (!args.includes(item)) {
+        finalArray.push(item);
+        }
+    }); 
+    return finalArray;
+  };
 
 module.exports = removeFromArray
   
+
+//removeFromArray([1, 2, 3, 4], 3); // should remove 3 and return [1,2,4]
