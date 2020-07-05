794d98bb3fc31e5a02401dde57dea74f08780912
Rebecca Hawkenson <rehawkenson@gmail.com>
2020-07-05 10:24:19 -0500

    Complete the function to pass test parameters

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    let sum =0;
+    if (num1  < 0 || num2 < 0 || typeof(num1) !== 'number' || typeof(num2) !== 'number' ){
+        return 'ERROR';    
+    } else if (num1 < num2){
+        for (let i=num1; i<=num2; i++){
+            sum += i;
+        } return sum;
+    } else if (num1 > num2){
+        for (let i=num2; i<=num1; i++){
+            sum += i;
+        } return sum;
+    } else {
+        return 'ERROR'
+    }
 }
 
 module.exports = sumAll
