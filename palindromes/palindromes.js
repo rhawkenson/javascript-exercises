1cbe6d3ce9d0faa114b2e98b93b27b86174880dd
Rebecca Hawkenson <rehawkenson@gmail.com>
2020-07-21 17:26:18 -0500

    Look up the RegEx to filter out punctuation so that the function is only paying attention to the letters

--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function() {
-
+const palindromes = function(str) {
+    let re = /[\W_]/g;
+    let editStr = str.toLowerCase().replace(re, '');
+    let reverseString = editStr.split('').reverse().join('');
+    return reverseString === editStr;
 }
 
 module.exports = palindromes
