c8840d4266d93e291655ca206916f49f69e9a385
Rebecca Hawkenson <rehawkenson@gmail.com>
2020-07-23 15:05:44 -0500

    map a new array that pulls out data listed under title in the passed array

--- getTheTitles/getTheTitles.js
+++ getTheTitles/getTheTitles.js
@@ -1,5 +1,8 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(bookArr) {
+    let newArr = bookArr.map(books => books.title);
+    return newArr;
 }
 
 module.exports = getTheTitles;
+
+//  jasmine getTheTitles.spec.js
