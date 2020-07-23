eb4f57fa9081d14e4dff99c39836ca7e32b9b4d0
Rebecca Hawkenson <rehawkenson@gmail.com>
2020-07-23 14:28:11 -0500

    Create algorithms for negative and positive value, respectively. The upper case values need to be seperated from the lowercase value to maintain casing, but the inserted arithmatic is the same. Convert the code of the letter to reflect 0-26 values, making sure to return them to UTF-16 values after adding or subtracting the key. Return everything to the string

--- caesar/caesar.js
+++ caesar/caesar.js
@@ -1,6 +1,41 @@
 
 const caesar = function(msg, key) {
-     
+    let results = '';
+    for (let i=0; i<msg.length; i++){ 
+        let value = msg.charCodeAt(i);
+
+        if(value >= 65 && value <= 90){
+            let alphaValue = value - 64;
+            if((alphaValue + key) > 26){ 
+                newLetter = ((key%26)-(26-alphaValue));
+                results += String.fromCharCode(newLetter + 64);
+
+            } else if((alphaValue + key) < 0) {
+                newLetter = ((26+alphaValue)+(key%26))%26;
+                results += String.fromCharCode(newLetter + 64);
+            } else {
+                let newCode = value + key;
+                results += String.fromCharCode(newCode);
+            }
+
+        } else if (value >= 97 && value <= 122){
+            let alphaValue = value - 96;
+            if((alphaValue + key) > 26){
+                newLetter = ((key%26)-(26-alphaValue));
+                results += String.fromCharCode(newLetter + 96);
+                
+            } else if((alphaValue + key) < 0) {
+                    newLetter = ((26+alphaValue)+(key%26))%26;
+                    results += String.fromCharCode(newLetter + 96);
+            } else {
+                let newCode = value + key;
+                results += String.fromCharCode(newCode);
+            }
+        } else {
+            results += String.fromCharCode(value);
+        }
+    } return results;
+
 }
 
 
@@ -9,3 +44,4 @@ module.exports = caesar
 
 
 //       jasmine caesar.spec.js
+
