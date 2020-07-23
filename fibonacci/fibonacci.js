1124662c8f3d97cd0c806038808c3872f207399c
Rebecca Hawkenson <rehawkenson@gmail.com>
2020-07-23 10:45:06 -0500

    Add logic that shifts the current value and added value to the added variables, and pushes the sum to an arry. Call the array value at the index of the one less than the num given to account for 0 index in arrays.

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,5 +1,22 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    let current = 0;
+    let newNum = 1;
+    let resultsArr = [1];
+    
+    if (!Number(num) || num < 0){
+        return 'OOPS';
+    
+    } else {
+        for (let i=0; i<=num; i++){
+        let value = current + newNum;
+        resultsArr.push(value);
+        current = newNum;
+        newNum = value;
+        
+        } 
+    }return resultsArr[num-1];
 }
 
 module.exports = fibonacci
+
+//       jasmine fibonacci.spec.js
